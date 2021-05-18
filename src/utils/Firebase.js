e8d7c6a5d56647c2eac93c23dc34341dab0b4928
Ezequiel Moreira <ezequielmoreira@cpejr.com>
2021-05-17 21:10:47 -0300

    Adicionei um try catch

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -12,18 +12,24 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 module.exports = {
-    async createNewUser(email, password) {
-        const result = await firebase
-            .auth()
-            .createUserWithEmailAndPassword(email,password);
-        return result.user.uid;
-    },
+async createNewUser(email, password){
+   try { 
+    const result = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email,password);
+
+    return result.user.uid;
+
+   } catch (error) {
+       console.warn(error);
+   }
+   },
 
     async login(email, password){
+       
         const result = await firebase
             .auth()
             .signInWithEmailAndPassword(email,password);
         return result.user.uid;
     },
-  
 };
