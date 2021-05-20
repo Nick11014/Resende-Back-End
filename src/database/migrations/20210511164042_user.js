9ec4fc2366b7c84e4feffb3164c8569dac2d16fc
Matheus <matheusmiranda98@live.com>
2021-05-20 14:41:39 -0300

    Reorganização das migrations

--- src/database/migrations/20210511164042_user.js
+++ src/database/migrations/20210511164042_user.js
@@ -1,9 +1,17 @@
 exports.up = function (knex) {
   return knex.schema.createTable("user", function (table) {
     table.string("user_id").primary().notNullable();
+    table.string("NomeEmp").notNullable();
     table.string("email").notNullable();
-    table.string("senha").notNullable();
     table.string("firebase_id").notNullable();
+    table.string("tipo").notNullable();
+    table.string("aae").notNullable();
+    table.string("tel").notNullable();
+    table.string("end").notNullable();
+    table.string("num").notNullable();
+    table.string("comp").notNullable();
+    table.string("desc").notNullable();
+    table.string("cnpj").notNullable();
   });
 };
 
