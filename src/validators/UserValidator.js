decec985f10096645a439c13891eea6f64c6b4fe
Matheus <matheusmiranda98@live.com>
2021-05-26 22:47:49 -0300

    Atualização dos requisitos.

--- src/validators/UserValidator.js
+++ src/validators/UserValidator.js
@@ -3,16 +3,16 @@ const { celebrate, Segments, Joi } = require("celebrate");
 module.exports = {
   create: celebrate({
     [Segments.BODY]: Joi.object().keys({
-      email: Joi.string().email().required(),
-      password: Joi.string().min(6).required(),
-      tipo: Joi.string().required(),
-      NomeEmp: Joi.string().required(),
-      aae: Joi.string().required(),
-      tel: Joi.number().integer().min(10000000000).max(99999999999999).required(),
-      end: Joi.string().required(),
+      email: Joi.string().email().min(3).max(30).required(),
+      password: Joi.string().min(6).max(20).required(),
+      tipo: Joi.string().min(3).max(30).required(),
+      NomeEmp: Joi.string().min(3).max(30).required(),
+      aae: Joi.string().min(3).max(30).required(),
+      tel: Joi.number().integer().min(10000000000).max(99999999999).required(),
+      end: Joi.string().min(3).max(50).required(),
       num: Joi.number().integer().min(0).max(9999).required(),
-      comp: Joi.string().required(),
-      desc: Joi.string().required(),
+      comp: Joi.string().min(3).max(30).required(),
+      desc: Joi.string().min(3).max(600).required(),
       cnpj: Joi.number().integer().min(10000000000).max(99999999999999).required(),
     }),
   }),
@@ -24,17 +24,22 @@ module.exports = {
   update: celebrate({
     [Segments.BODY]: Joi.object()
       .keys({
-        NomeEmp: Joi.string().optional(),
-        tipo: Joi.string().optional(),
-        aae: Joi.string().optional(),
+        NomeEmp: Joi.string().min(3).max(30).optional(),
+        tipo: Joi.string().min(3).max(30).optional(),
+        aae: Joi.string().min(3).max(30).optional(),
         tel: Joi.number().integer().min(10000000000).max(99999999999).optional(),
-        end: Joi.string().optional(),
+        end: Joi.string().min(3).max(50).optional(),
         num: Joi.number().integer().min(0).max(9999).optional(),
-        comp: Joi.string().optional(),
-        desc: Joi.string().optional(),
+        comp: Joi.string().min(3).max(30).optional(),
+        desc: Joi.string().min(3).max(600).optional(),
         cnpj: Joi.number().integer().min(10000000000).max(99999999999999).optional(),
       })
       .min(1),
+      [Segments.HEADERS]: Joi.object()
+      .keys({
+        authorization: Joi.string().required(),
+      })
+      .unknown(),
   }),
   delete: celebrate({
     [Segments.PARAMS]: Joi.object().keys({
@@ -43,5 +48,10 @@ module.exports = {
     [Segments.BODY]: Joi.object().keys({
       email: Joi.string().email().required(),
     }),
+    [Segments.HEADERS]: Joi.object()
+      .keys({
+        authorization: Joi.string().required(),
+      })
+      .unknown(),
   }),
 };
